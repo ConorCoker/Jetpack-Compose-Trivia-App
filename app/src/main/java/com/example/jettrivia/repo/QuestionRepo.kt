913356b074963d1d81efe705e77894c61270f0bc
Conor Coker <conorcoker@gmail.com>
2023-05-23 15:04:09 +0100

    Added getAllQuestions() method in repo that returns our wrapper class containing our list of questions or Exception, this method is called from our new QuestionsViewModel class and set to be a MutableState which can then be accessed and used to manipulate ui

--- app/src/main/java/com/example/jettrivia/repo/QuestionRepo.kt
+++ app/src/main/java/com/example/jettrivia/repo/QuestionRepo.kt
@@ -1,5 +1,6 @@
 package com.example.jettrivia.repo
 
+import android.util.Log
 import com.example.jettrivia.data.DataOrException
 import com.example.jettrivia.model.QuestionItem
 import com.example.jettrivia.network.QuestionApi
@@ -7,6 +8,19 @@ import javax.inject.Inject
 
 class QuestionRepo @Inject constructor(private val api: QuestionApi) {
 
-    private val listOfQuestions = DataOrException<ArrayList<QuestionItem>, Boolean, Exception>()
+    private val dataOrException = DataOrException<ArrayList<QuestionItem>, Boolean, Exception>()
+
+    suspend fun getAllQuestions(): DataOrException<ArrayList<QuestionItem>, Boolean, Exception> {
+        try {
+            dataOrException.loading = true
+            dataOrException.data = api.getAllQuestions() //getting arraylist of QuestionItem
+            if (dataOrException.data.toString().isNotEmpty()) dataOrException.loading = false //if its not empty we received some data so its no longer loading
+
+        } catch (exception: Exception){
+            dataOrException.e = exception //appending the exception to our wrapper class
+            Log.d("Exc","getAllQuestions: ${dataOrException.e!!.localizedMessage}")
+        }
+        return dataOrException
+    }
 
 }
---
+++ app/src/main/java/com/example/jettrivia/screens/QuestionsViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.jettrivia.screens
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.jettrivia.data.DataOrException
+import com.example.jettrivia.model.QuestionItem
+import com.example.jettrivia.repo.QuestionRepo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class QuestionsViewModel @Inject constructor(private val repo: QuestionRepo) : ViewModel() {
+
+    val data: MutableState<DataOrException<ArrayList<QuestionItem>, Boolean, Exception>> =
+        mutableStateOf(
+            DataOrException(
+                null,
+                true,
+                Exception("")
+            )
+        ) //holds mutable state of our data or exception so ui can react.
+
+    init {
+        getAllQuestions()
+    }
+
+    private fun getAllQuestions() {
+        viewModelScope.launch {
+            data.value.loading = true
+            data.value = repo.getAllQuestions()
+            if (data.value.data.toString().isNotEmpty()) {
+                data.value.loading = false
+            }
+
+        }
+    }
+}
